f0c7430f07b6fb6e8f4c243014b9b6dbc50f68ba
aasuyadav1 <aasuy284@gmail.com>
2024-05-07 18:14:19 +0530

    fix: bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {NextUIProvider} from '@nextui-org/react'
+import { Analytics } from "@vercel/analytics/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     
     <NextUIProvider>
     <App />
+    <Analytics />
     </NextUIProvider>
     
   </React.StrictMode>,
