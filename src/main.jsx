f507084cf13609724a241e548cdc329aa0c72f9c
aasuyadav1 <aasuy284@gmail.com>
2024-09-04 18:40:10 +0530

    Add: title and favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {NextUIProvider} from '@nextui-org/react'
-import { Analytics } from "@vercel/analytics/react"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { Analytics } from "@vercel/analytics/react";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
     <NextUIProvider>
-    <App />
-    <Analytics />
+      <App />
+      <Analytics />
     </NextUIProvider>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
