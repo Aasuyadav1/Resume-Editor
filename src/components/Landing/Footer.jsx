5671d442189eaf9c72d235d591b6fed15242001e
Aasu yadav <143898193+Aasuyadav1@users.noreply.github.com>
2025-01-12 21:04:24 +0530

    Update Footer.jsx

--- src/components/Landing/Footer.jsx
+++ src/components/Landing/Footer.jsx
@@ -6,7 +6,7 @@ import { FaGithub } from "react-icons/fa";
 function Footer() {
   return (
     <div className='md:mt-28 mt-14 py-3 flex justify-center items-center px-2 gap-4 md:justify-between md:gap-20 flex-wrap md:px-20'>
-   <p className='text-lg'> © 2024 all rights reserved</p>
+   <p className='text-lg'> © {new Date().getFullYear()} all rights reserved</p>
    {/* <p className='text-lg'>Developed by Aasu 💖</p> */}
     <div className='text-xl flex items-center gap-3'>
       <a href="https://www.linkedin.com/in/aasu-yadavv/" target='_blank'><CiLinkedin /></a>
